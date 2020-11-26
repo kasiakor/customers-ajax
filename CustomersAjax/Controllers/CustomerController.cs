ce8d6f46b60a089c986993203abc67d81e6b5a48
kasia.kordys <kasia.kordys@gmail.com>
2020-11-26 19:46:21 +0000

    add views

---
+++ CustomersAjax/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomersAjax.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            //get data, no db, hard-coded
+
+
+
+
+            return View("Customer");
+        }
+    }
+}
