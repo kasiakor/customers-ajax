c04fe5b3265c1f6d1323eb930712015e24f97b0e
kasia.kordys <kasia.kordys@gmail.com>
2020-11-27 14:33:41 +0000

    add tuple

--- CustomersAjax/Controllers/CustomerController.cs
+++ CustomersAjax/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomersAjax.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,31 @@ namespace CustomersAjax.Controllers
 {
     public class CustomerController : Controller
     {
+        //get data, no db, hard-coded
+        //define data for the app, customer object
+        Customer customer;
+        List<Customer> customers;
+
+
+        public CustomerController()
+        {
+            customers = new List<Customer>();
+            customers.Add(new Customer(0, "Anna", 34));
+            customers.Add(new Customer(1, "Brad", 23));
+            customers.Add(new Customer(2, "Charles", 48));
+            customers.Add(new Customer(3, "Daniel", 12));
+            customers.Add(new Customer(4, "Evelyn", 85));
+            customers.Add(new Customer(5, "Frank", 58));
+            customers.Add(new Customer(3, "Gia", 3));
+        }
         // GET: Customer
         public ActionResult Index()
         {
-            //get data, no db, hard-coded
+            Tuple<List<Customer>, Customer> tuple;
+            tuple = new Tuple<List<Customer>, Customer>(customers, customers[0]);
 
-
-
-
-            return View("Customer");
+            //tuple - object model
+            return View("Customer", tuple);
         }
     }
 }
